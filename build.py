194a82dd607e53a0dcc2c4be2f736bd2be6a47df
yjjnls <x-jj@foxmail.com>
2018-06-22 21:40:42 +0800

    add environments

--- build.py
+++ build.py
@@ -2,8 +2,15 @@
 # -*- coding: utf-8 -*-
 
 from bincrafters import build_template_default
+import os
 
 if __name__ == "__main__":
+    CONAN_USERNAME = os.environ.get("CONAN_USERNAME")
+    CONAN_UPLOAD = 'https://api.bintray.com/conan/%s/%s' % (CONAN_USERNAME,
+                                                            'stable')
+    os.environ['CONAN_UPLOAD'] = CONAN_UPLOAD
+    os.environ['CONAN_CHANNEL'] = 'stable'
+    os.environ['CONAN_UPLOAD_ONLY_WHEN_STABLE'] = 'False'
 
     builder = build_template_default.get_builder()
 
