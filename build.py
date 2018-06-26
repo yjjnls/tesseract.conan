bbe333550beff1ea315c35125d78cfe2085d3ab6
yuanjunjie <yuanjunjie@kedacom.com>
2018-06-26 09:55:23 +0800

    build shared library only

--- build.py
+++ build.py
@@ -3,6 +3,7 @@
 
 from bincrafters import build_template_default
 import os
+import platform
 
 if __name__ == "__main__":
     CONAN_USERNAME = os.environ.get("CONAN_USERNAME")
@@ -13,5 +14,26 @@ if __name__ == "__main__":
     os.environ['CONAN_UPLOAD_ONLY_WHEN_STABLE'] = 'False'
 
     builder = build_template_default.get_builder()
+    builds = []
+    for settings, options, env_vars, build_requires, reference in builder.items:
+        # dynamic only
+        if not options["tesseract:shared"]:
+            continue
+        # release only
+        if settings["build_type"] == "Debug":
+            continue
+
+        # Visual Sutido 2017 only
+        if platform.system() == "Windows":
+            if settings["compiler"] == "Visual Studio":
+                if settings["compiler.version"] == '14':
+                    builds.append(
+                        [settings, options, env_vars, build_requires])
+        elif platform.system() == "Linux":
+            if settings["compiler"] == "gcc":
+                if settings["compiler.version"] == '4.9' and settings["arch"] == 'x86_64':
+                    builds.append(
+                        [settings, options, env_vars, build_requires])
+    builder.builds = builds
 
     builder.run()
